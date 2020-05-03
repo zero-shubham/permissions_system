c9a23a07920b8bfabe046bbfad8390f026c3fd91
Shubham Biswas <shubhambiswas.zero@gmail.com>
2020-05-03 18:48:18 +0530

    version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="permissions-system",
-    version="0.1.0",
+    version="0.1.1",
     description="Alternative to admin utilities for more autonomy, targeted towards FastAPI.",
     long_description=README,
     long_description_content_type="text/markdown",
